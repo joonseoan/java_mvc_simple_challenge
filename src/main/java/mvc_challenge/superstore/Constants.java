d0f7136c64245f0a9d77b0b16705eef2e18eda0a
Joon Seo An <joonseo.an@outlook.com>
2023-03-22 08:59:29 -0400

    added flash note

--- src/main/java/mvc_challenge/superstore/Constants.java
+++ src/main/java/mvc_challenge/superstore/Constants.java
@@ -1,5 +1,8 @@
 package mvc_challenge.superstore;
 
 public class Constants {
+  public static final int NOT_FOUND = -1;
+  public static final String SUCCESS = "success";
+  public static final String FAIL = "fail";
   public static final String[] CATEGORIES = { "Furniture", "Office Supplies", "Technology" };
 }
--- src/main/java/mvc_challenge/superstore/SuperstoreController.java
+++ src/main/java/mvc_challenge/superstore/SuperstoreController.java
@@ -5,10 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.net.http.HttpClient;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class SuperstoreController {
@@ -34,14 +38,30 @@ public class SuperstoreController {
   }
 
   @PostMapping("/handleSubmit")
-  public String handleOrderSubmit(Order order) {
+  public String handleOrderSubmit(Order order, RedirectAttributes redirectAttributes) {
     int index = getIndex(order.getId());
+    String flashString;
 
     if (index > -1) {
-      orders.set(index, order);
+      Date newDate = order.getDate();
+      Date oldDate = orders.get(index).getDate();
+
+      if (!within5Days(newDate, oldDate)) {
+        flashString = Constants.FAIL;
+
+      } else {
+        flashString = Constants.SUCCESS;
+        orders.set(index, order);
+      }
+
     } else {
+      flashString = Constants.SUCCESS;
       orders.add(order);
     }
+
+
+    // add a flash attribute
+    redirectAttributes.addFlashAttribute("status", flashString);
 
     return "redirect:/inventory";
   }
@@ -50,6 +70,7 @@ public class SuperstoreController {
   public String getInventory(Model model) {
     model.addAttribute("orders", orders);
 
+    // [IMPORTANT] flashAttributes with key name, "status" will be automatically added into `model`
     return "inventory";
   }
 
@@ -60,6 +81,13 @@ public class SuperstoreController {
       }
     }
 
-    return -1;
+    return Constants.NOT_FOUND;
+  }
+
+  private boolean within5Days(Date newDate, Date oldDate) {
+    // getTime() with milliseconds
+    long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+    // milliseconds to day
+    return TimeUnit.MILLISECONDS.toDays(diff) <= 5;
   }
 }
